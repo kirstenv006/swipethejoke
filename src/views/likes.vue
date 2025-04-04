2d36ab2f59e7f7542dd539b8c64dd607bb219d7d
kirstenv006 <kirstenmveenstra@outlook.com>
2025-04-04 16:40:34 +0200

    Opslaan van likes

--- src/views/likes.vue
+++ src/views/likes.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="Likes">
     <h1>Bekijk je likes</h1>
-    <div v-for="like in likes" :key="like.id">
+    <div v-for="(like, index) in likes" :key="index">
       <p>{{ like }}</p>
-  </div>
+      <button @click="verwijderLike(index)">Delete like</button>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
@@ -16,15 +16,16 @@ export default {
     }
   },
   mounted() {
-    // this.likes = this.store.getters.allItems;
-
     const grapjes = JSON.parse(localStorage.getItem("jokes")) || [];
     this.likes = grapjes;
-
+  },
+  methods: {
+    verwijderLike(index) {
+      this.likes.splice(index, 1); 
+      localStorage.setItem("jokes", JSON.stringify(this.likes)); 
+    }
   }
 }
-
-
 </script>
 
 <style>
@@ -36,8 +37,8 @@ export default {
     font-size: 15px;
   }
 
- h1{
+  h1 {
     width: 100%; 
     margin-bottom: 2rem;
- }
+  }
 </style>
