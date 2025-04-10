1f91b011622097a19c50200f9ea7cd76dbc63776
kirstenv006 <kirstenmveenstra@outlook.com>
2025-04-10 20:43:15 +0200

    startpagina gerepareerd

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
     <div class="wrapper">
       <nav>
-        <RouterLink to="/joke">Swipe</RouterLink>
+        <RouterLink to="/">Swipe</RouterLink>
         <RouterLink to="/likes">Likes</RouterLink>
       </nav>
     </div>
